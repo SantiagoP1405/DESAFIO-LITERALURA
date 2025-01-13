ada069d74e7fd6d1574dee00d7904078ce2d70ca
Santiago Gómez <santipatito04@gmail.com>
2025-01-13 11:19:39 -0600

    Class Principal
    Se creó la clase principal con atributos y métodos de prueba para verificar que el programa se está conectando a la base de datos

--- literalura/src/main/java/com/santiagogomez/literalura/LiteraluraApplication.java
+++ literalura/src/main/java/com/santiagogomez/literalura/LiteraluraApplication.java
@@ -1,13 +1,22 @@
 package com.santiagogomez.literalura;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.santiagogomez.literalura.principal.Principal;
 
 @SpringBootApplication
-public class LiteraluraApplication {
+public class LiteraluraApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.pruebaAPI();
+
 	}
 
 }
---
+++ literalura/src/main/java/com/santiagogomez/literalura/principal/Principal.java
@@ -0,0 +1,20 @@
+package com.santiagogomez.literalura.principal;
+
+import java.util.Scanner;
+
+import com.santiagogomez.literalura.service.ConsumoAPI;
+import com.santiagogomez.literalura.service.ConvierteDatos;
+
+public class Principal {
+    private Scanner input = new Scanner(System.in);
+    private ConsumoAPI consumoAPI = new ConsumoAPI();
+    private final String url = "https://gutendex.com/books";
+    private ConvierteDatos convierteDatos = new ConvierteDatos();
+
+    public void pruebaAPI(){
+        System.out.println("Estos son los datos de la API");
+        var json = consumoAPI.obtenerDatos(url);
+        System.out.println(json);
+    }
+    
+}
