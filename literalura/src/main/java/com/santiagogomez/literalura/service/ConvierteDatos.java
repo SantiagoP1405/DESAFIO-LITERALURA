553d33e324c5ebc81d285cab41f94871846da4e5
Santiago Gómez <santipatito04@gmail.com>
2025-01-13 10:54:12 -0600

    Save ConvierteDatos

--- literalura/src/main/java/com/santiagogomez/literalura/service/ConvierteDatos.java
+++ literalura/src/main/java/com/santiagogomez/literalura/service/ConvierteDatos.java
@@ -3,16 +3,16 @@ package com.santiagogomez.literalura.service;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-public class ConvierteDatos {
+public class ConvierteDatos implements IConverteDatos{
     private ObjectMapper objectMapper = new ObjectMapper();
 
+    @Override
     public <T> T obtenerDatos(String json, Class<T> clase) {
         try{
             return objectMapper.readValue(json, clase);
         }
         catch(JsonProcessingException e){
             throw new RuntimeException(e);
-        }
-        
+        }    
     }
 }
