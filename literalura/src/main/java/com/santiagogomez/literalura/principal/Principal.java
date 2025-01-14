a955673b22ee0ad185fae90c20cd52995a085498
Santiago Gómez <santipatito04@gmail.com>
2025-01-13 22:16:49 -0600

    Eliminación pruebaAPI
    Se eliminó el método de pruebaAPI, ya que ya existen otros métodos que implementan las mismas funciones

--- literalura/src/main/java/com/santiagogomez/literalura/principal/Principal.java
+++ literalura/src/main/java/com/santiagogomez/literalura/principal/Principal.java
@@ -149,53 +149,16 @@ public class Principal {
             System.out.println("No se encontró un libro con ese nombre.");
         }
     }
-    
-
-    public void pruebaAPI() {
-        System.out.println("Estos son los datos de la API");
-        var json = consumoAPI.obtenerDatos(url);
-        Results datos = convierteDatos.obtenerDatos(json, Results.class);
-        libros = datos.getResults();
-
-        mostrarLibrosBuscados();
-        System.out.println("Escribe el nombre del libro que quieres buscar:");
-        var nombreLibro = input.nextLine().toLowerCase(); // Convertimos a minúsculas para la búsqueda
-
-        Optional<Libro> libroEncontrado = libros.stream()
-                .filter(libro -> libro.getTitulo().toLowerCase().contains(nombreLibro)) // Insensible a mayúsculas
-                .findFirst();
-
-        if (libroEncontrado.isPresent()) {
-            var libro = libroEncontrado.get();
-
-            Optional<Libro> libroEnBaseDeDatos = libroRepository.findById(libro.getId());
-            if (libroEnBaseDeDatos.isPresent()) {
-                System.out.println("El libro con el nombre '" + libro.getTitulo() + "' ya existe en la base de datos.");
-            } else {
-                procesarLibro(libro);
-            }
-        } else {
-            System.out.println("No se encontró un libro con ese nombre.");
-        }
-    }
 
     private void procesarLibro(Libro libro) {
-        // Actualizar o crear autores
         List<Autor> autoresActualizados = libro.getAutores().stream()
             .map(autor -> {
                 Optional<Autor> autorExistente = autorRepository.findByNombre(autor.getNombre());
                 return autorExistente.orElseGet(() -> autorRepository.save(autor));
             }).collect(Collectors.toList());
-
-        // Asociar autores actualizados al libro
         libro.setAutores(autoresActualizados);
-
-        // Guardar libro con autores actualizados
         libroRepository.save(libro);
         System.out.println("Libro guardado: " + libro.getTitulo());
     }
-
-    
-
     
 }
